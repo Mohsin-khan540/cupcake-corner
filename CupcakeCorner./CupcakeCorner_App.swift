175b8ca232a426c23ec17c208f3cf7fd66d1970f
Mohsin-khan540 <arhamk4555@gmail.com>
2025-09-09 22:32:13 +0500

    Initial Commit

---
+++ CupcakeCorner./CupcakeCorner_App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_App.swift
+//  CupcakeCorner.
+//
+//  Created by Mohsin khan on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner.Tests/CupcakeCorner_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_Tests.swift
+//  CupcakeCorner.Tests
+//
+//  Created by Mohsin khan on 09/09/2025.
+//
+
+import Testing
+@testable import CupcakeCorner_
+
+struct CupcakeCorner_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
